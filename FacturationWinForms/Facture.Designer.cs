06de9986a2875fbc84601c89290387d4980f6fb9
JOSUE <JOSUE@DESKTOP-C8BKLAT>
2021-11-23 13:33:34 +0100

    Ajoutez des fichiers projet.

---
+++ FacturationWinForms/Facture.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace FacturationWinForms
+{
+    partial class Facture
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPrixTotal = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPrixunitaire = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtQuantite = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDesignation = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtReference = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PrixUnitaire = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Quantite = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Designation = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Reference = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.PrixTotal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Nom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPrixTotal
+            // 
+            this.txtPrixTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrixTotal.Location = new System.Drawing.Point(430, 195);
+            this.txtPrixTotal.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrixTotal.Name = "txtPrixTotal";
+            this.txtPrixTotal.Size = new System.Drawing.Size(160, 32);
+            this.txtPrixTotal.TabIndex = 31;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(320, 202);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 24);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Prix Total";
+            // 
+            // txtPrixunitaire
+            // 
+            this.txtPrixunitaire.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrixunitaire.Location = new System.Drawing.Point(430, 140);
+            this.txtPrixunitaire.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrixunitaire.Name = "txtPrixunitaire";
+            this.txtPrixunitaire.Size = new System.Drawing.Size(160, 32);
+            this.txtPrixunitaire.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(320, 148);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 24);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Prix Unitaire";
+            // 
+            // txtQuantite
+            // 
+            this.txtQuantite.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantite.Location = new System.Drawing.Point(130, 140);
+            this.txtQuantite.Margin = new System.Windows.Forms.Padding(2);
+            this.txtQuantite.Name = "txtQuantite";
+            this.txtQuantite.Size = new System.Drawing.Size(160, 32);
+            this.txtQuantite.TabIndex = 27;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(26, 148);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 24);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Quantite";
+            // 
+            // txtDesignation
+            // 
+            this.txtDesignation.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDesignation.Location = new System.Drawing.Point(430, 75);
+            this.txtDesignation.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDesignation.Name = "txtDesignation";
+            this.txtDesignation.Size = new System.Drawing.Size(160, 32);
+            this.txtDesignation.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(320, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 24);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Designation";
+            // 
+            // txtReference
+            // 
+            this.txtReference.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReference.Location = new System.Drawing.Point(130, 75);
+            this.txtReference.Margin = new System.Windows.Forms.Padding(2);
+            this.txtReference.Name = "txtReference";
+            this.txtReference.Size = new System.Drawing.Size(160, 32);
+            this.txtReference.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 82);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 24);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Reference";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(446, 376);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 39);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Confirmer";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // PrixUnitaire
+            // 
+            this.PrixUnitaire.Text = "Prix Unitaire";
+            this.PrixUnitaire.Width = 100;
+            // 
+            // Quantite
+            // 
+            this.Quantite.Text = "Quantite";
+            this.Quantite.Width = 100;
+            // 
+            // Designation
+            // 
+            this.Designation.Text = "Designation";
+            this.Designation.Width = 100;
+            // 
+            // Reference
+            // 
+            this.Reference.Text = "Reference";
+            this.Reference.Width = 100;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Reference,
+            this.Designation,
+            this.Quantite,
+            this.PrixUnitaire,
+            this.PrixTotal});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(11, 256);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(378, 159);
+            this.listView1.TabIndex = 20;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // PrixTotal
+            // 
+            this.PrixTotal.Text = "Prix Total";
+            this.PrixTotal.Width = 100;
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Email.Location = new System.Drawing.Point(436, 5);
+            this.txt_Email.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(160, 32);
+            this.txt_Email.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(352, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 29);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Email";
+            // 
+            // txt_Nom
+            // 
+            this.txt_Nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nom.Location = new System.Drawing.Point(93, 7);
+            this.txt_Nom.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Nom.Name = "txt_Nom";
+            this.txt_Nom.Size = new System.Drawing.Size(160, 32);
+            this.txt_Nom.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 29);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Nom";
+            // 
+            // Facture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPrixTotal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPrixunitaire);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtQuantite);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDesignation);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtReference);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Nom);
+            this.Controls.Add(this.label1);
+            this.Name = "Facture";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Facture_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPrixTotal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPrixunitaire;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtQuantite;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDesignation;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtReference;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader PrixUnitaire;
+        private System.Windows.Forms.ColumnHeader Quantite;
+        private System.Windows.Forms.ColumnHeader Designation;
+        private System.Windows.Forms.ColumnHeader Reference;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader PrixTotal;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Nom;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
